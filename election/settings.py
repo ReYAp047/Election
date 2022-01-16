5de94f180159d7098916c40bdf739ab553353e5a
Dali47 <meftah.dali@gmail.com>
2022-01-16 15:55:45 +0100

    Préparation du template

--- election/settings.py
+++ election/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import cloudinary
 import cloudinary.uploader
@@ -73,6 +74,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 WSGI_APPLICATION = 'election.wsgi.application'
